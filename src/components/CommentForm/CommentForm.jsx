50c54f79b684fb12621709cc3c02fb744ce3d6e5
Ashley Sands <ashley.sands01@gmail.com>
2026-02-08 23:05:55 -0800

    remove cde comment

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const CommentForm = ({ handleAddComment }) => {
-  const [formData, setFormData] = useState({ text: '' });
+    const [formData, setFormData] = useState({ text: '' })
 
-  const handleChange = (evt) => {
-    setFormData({ ...formData, [evt.target.name]: evt.target.value });
-  };
+    const handleChange = (evt) => {
+        setFormData({ ...formData, [evt.target.name]: evt.target.value })
+    }
 
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    // add handleAddComment
-    handleAddComment(formData)
-    setFormData({ text: '' });
-  };
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+        handleAddComment(formData)
+        setFormData({ text: '' })
+    }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor='text-input'>Your comment:</label>
-      <textarea
-        required
-        type='text'
-        name='text'
-        id='text-input'
-        value={formData.text}
-        onChange={handleChange}
-      />
-      <button type='submit'>SUBMIT COMMENT</button>
-    </form>
-  );
-};
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor='text-input'>Your comment:</label>
+            <textarea
+                required
+                type='text'
+                name='text'
+                id='text-input'
+                value={formData.text}
+                onChange={handleChange}
+            />
+            <button type='submit'>SUBMIT COMMENT</button>
+        </form>
+    )
+}
 
-export default CommentForm;
-
+export default CommentForm
